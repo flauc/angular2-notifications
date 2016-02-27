bf78551f7327b1196fd6669fa634433f0a767cb9
flauc <filip.lauc93@gmail.com>
2016-02-27 22:28:19 +0100

    Working on notifications

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,5 @@
 import {Component, enableProdMode} from 'angular2/core';
-import {NotificationsService, NotificationsComponent} from 'notifications/components'
+import {NotificationsService, NotificationsComponent} from 'notifications'
 import {bootstrap} from 'angular2/platform/browser';
 enableProdMode();
 
