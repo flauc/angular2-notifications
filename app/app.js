c42030f41e2f911ac5754cf900ed1e2ccabd6acb
flauc <filip.lauc93@gmail.com>
2016-02-28 00:52:38 +0100

    Working on page

--- app/app.js
+++ app/app.js
@@ -1,27 +1,41 @@
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var core_1 = require('angular2/core');
-var components_1 = require('notifications/components');
-var browser_1 = require('angular2/platform/browser');
-core_1.enableProdMode();
-var AppComponent = (function () {
-    function AppComponent() {
-        this.test = 'test';
+System.register(['angular2/core', 'notifications/components', 'angular2/platform/browser'], function(exports_1) {
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var core_1, components_1, browser_1;
+    var AppComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (components_1_1) {
+                components_1 = components_1_1;
+            },
+            function (browser_1_1) {
+                browser_1 = browser_1_1;
+            }],
+        execute: function() {
+            core_1.enableProdMode();
+            AppComponent = (function () {
+                function AppComponent() {
+                    this.test = 'test';
+                }
+                AppComponent = __decorate([
+                    core_1.Component({
+                        selector: 'app',
+                        templateUrl: 'app/app.html',
+                        directives: [components_1.NotificationsComponent],
+                        providers: [components_1.NotificationsService]
+                    })
+                ], AppComponent);
+                return AppComponent;
+            })();
+            exports_1("AppComponent", AppComponent);
+            browser_1.bootstrap(AppComponent);
+        }
     }
-    AppComponent = __decorate([
-        core_1.Component({
-            selector: 'app',
-            templateUrl: 'app/app.html',
-            directives: [components_1.NotificationsComponent],
-            providers: [components_1.NotificationsService]
-        })
-    ], AppComponent);
-    return AppComponent;
-})();
-exports.AppComponent = AppComponent;
-browser_1.bootstrap(AppComponent);
-//# sourceMappingURL=app.js.map
+});
