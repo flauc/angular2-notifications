33c4ca4c1464638594464391fd9200e1963e559a
flauc <filip.lauc93@gmail.com>
2016-03-30 21:15:21 +0200

    chore: moved code from the master branch

---
+++ app/src/notificationType.js
@@ -0,0 +1,9 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters:[],
+        execute: function() {
+        }
+    }
+});
