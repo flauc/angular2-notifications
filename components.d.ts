9a8da0a4e0c26bba6c86f4923d5e4917b3f0a5b8
Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
2016-05-19 21:52:47 +0200

    change components.d.ts declarations

--- components.d.ts
+++ components.d.ts
@@ -1,2 +1,21 @@
-export * from './lib/simpleNotifications.component';
-export * from './lib/notifications.service';
+
+export declare class SimpleNotificationsComponent {
+}
+
+export declare class NotificationsService {
+  success(title: string, content: string, override?: any)
+
+  error(title: string, content: string, override?: any)
+
+  alert(title: string, content: string, override?: any)
+
+  info(title: string, content: string, override?: any)
+
+  bare(title: string, content: string, override?: any)
+
+  create(title: string, content: string, type: string, override?: any)
+
+  html(html: any, type: string, override?: any)
+
+  remove(id?: string)
+}
