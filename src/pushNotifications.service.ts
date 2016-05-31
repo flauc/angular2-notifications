94b80f7ab1a074362ffc17065a43fcf977775325
flauc <filip.lauc93@gmail.com>
2016-05-31 22:53:45 +0200

    feat: Working on push notifications

---
+++ src/pushNotifications.service.ts
@@ -0,0 +1,10 @@
+import {Injectable, EventEmitter} from "@angular/core"
+
+@Injectable
+export class PushNotificationsService {
+    public canCreate: boolean = false;
+
+    create({}): void {
+
+    }
+}
