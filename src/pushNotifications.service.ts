addc46a5f8ab33dd14fd8ef93a11644b9af14be8
flauc <filip.lauc93@gmail.com>
2016-05-31 23:14:02 +0200

    feat: Working on push notifications

--- src/pushNotifications.service.ts
+++ src/pushNotifications.service.ts
@@ -1,10 +1,55 @@
 import {Injectable, EventEmitter} from "@angular/core"
 
+declare var Notification;
+
 @Injectable
 export class PushNotificationsService {
-    public canCreate: boolean = false;
+    private canCreate: boolean = false;
+    private notificationBuffer: PushNotification;
 
-    create({}): void {
+    create(data: PushNotification): any {
+        if (this.canCreate) {
+            let notification = new Notification(data.title, {
+                body: data.body
+            });
 
+            return notification;
+        }
+
+        else {
+            this.notificationBuffer = data;
+            this.getReq()
+        }
     }
+
+    private getReq(): void {
+        if (!("Notification" in window)) {
+            console.log("This browser does not support desktop notification");
+            return;
+        }
+
+        if (Notification.permission === "granted") this.createBuffered();
+
+        else if (Notification.permission !== "denied") {
+            Notification.requestPermission()
+                .then(a => {
+                    if (a === "denied") console.log("Permission wasn't granted");
+                    else if (a === "default") console.log("The permission request was dismissed.")
+                    else this.createBuffered();
+                })
+        }
+    }
+
+    private createBuffered() {
+        this.canCreate = true;
+        if (this.notificationBuffer) {
+            this.create(this.notificationBuffer);
+            this.notificationBuffer = null;
+        }
+    }
+}
+
+interface PushNotification {
+    title: string
+    body: string
 }
