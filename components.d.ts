05148e9a9a3891265130f16f62a96d2043bdbdf6
Christopher Creighton <ccreighton@apptio.com>
2016-07-22 15:50:57 -0700

    Update exports & typings: - export+typings for Notification - fixed options export - updated return types for NotificationsService

--- components.d.ts
+++ components.d.ts
@@ -1,20 +1,27 @@
+export interface Notification {
+  id?: string;
+  type: string;
+  createdOn?: Date;
+  title?: string;
+  content?: string;
+  override?: any;
+  html?: any;
+}
+
 export declare class SimpleNotificationsComponent {}
 
 export declare class NotificationsService {
-    success(title: string, content: string, override?: any) :void
-    error(title: string, content: string, override?: any) :void
-    alert(title: string, content: string, override?: any) :void
-    info(title: string, content: string, override?: any) :void
-    bare(title: string, content: string, override?: any) :void
-    create(title: string, content: string, type: string, override?: any) :void
-    html(html: any, type: string, override?: any): void
-    remove(id?: string): void
+  success(title: string, content: string, override?: any): Notification;
+  error(title: string, content: string, override?: any): Notification;
+  alert(title: string, content: string, override?: any): Notification;
+  info(title: string, content: string, override?: any): Notification;
+  bare(title: string, content: string, override?: any): Notification;
+  create(title: string, content: string, type: string, override?: any): Notification;
+  html(html: any, type: string, override?: any): Notification;
+  remove(id?: string): void;
 }
 
 export declare class PushNotificationsService {
-    create(data: {title: string, body: string}): void
+  create(data: {title: string, body: string}): void
 }
-
-
-
 
--- components.js
+++ components.js
@@ -2,4 +2,6 @@ exports.SimpleNotificationsComponent = require('./lib/simpleNotifications.compon
 exports.NotificationsService = require('./lib/notifications.service').NotificationsService;
 exports.PushNotificationsService = require('./lib/pushNotifications.service').PushNotificationsService;
 exports.Icons = require('./lib/icons').Icons;
-exports.Icons = require('./lib/options').Options;
+exports.Options = require('./lib/options').Options;
+exports.Notification = require('./lib/notification').Notification;
+
