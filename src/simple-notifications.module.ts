3a7934951875a80714c43a21e43bc05599524d7c
flauc <filip.lauc93@gmail.com>
2016-09-02 09:39:40 +0200

    fix: Moved to CommonModule

--- src/simple-notifications.module.ts
+++ src/simple-notifications.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
+import {CommonModule} from '@angular/common';
 import {NotificationsService} from "./notifications.service";
 import {SimpleNotificationsComponent} from "./simple-notifications.component";
 import {NotificationComponent} from "./notification.component";
 import {MaxPipe} from "./max.pipe";
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [CommonModule],
     declarations: [SimpleNotificationsComponent, NotificationComponent, MaxPipe],
     providers: [NotificationsService],
     exports: [SimpleNotificationsComponent]
