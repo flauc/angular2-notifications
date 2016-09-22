bc3ac61ecc1e9f312de6debce242a2970407aebc
Filip Lauc <filip.lauc93@gmail.com>
2016-09-22 22:35:34 +0200

    chore: Refactoring

--- components.js
+++ components.js
@@ -1,8 +1,11 @@
 exports.SimpleNotificationsModule = require('./lib/simple-notifications.module').SimpleNotificationsModule;
 exports.SimpleNotificationsComponent = require('./lib/simple-notifications.component').SimpleNotificationsComponent;
+exports.NotificationComponent = require('./lib/notification.component').NotificationComponent;
 exports.NotificationsService = require('./lib/notifications.service').NotificationsService;
 exports.PushNotificationsService = require('./lib/push-notifications.service').PushNotificationsService;
 exports.Icons = require('./lib/icons').Icons;
-exports.Options = require('./lib/options').Options;
-exports.Notification = require('./lib/notification').Notification;
-
+exports.Options = require('./lib/options.type').Options;
+exports.Notification = require('./lib/notification.type').Notification;
+exports.NotificationEvent = require('./lib/notification-event.type').NotificationEvent;
+exports.NotificationType = require('./lib/notification-type.type').NotificationType;
+exports.MaxPipe = require('./lib/max.pipe').MaxPipe;
