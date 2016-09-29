d5eea3f4e34b740f539554f2812d23fad9e9d690
Filip Lauc <filip.lauc93@gmail.com>
2016-09-29 18:35:35 +0200

    test: Finished notifications.service.spec

--- src/notification.type.ts
+++ src/notification.type.ts
@@ -1,12 +1,18 @@
 export interface Notification {
-  id?: string;
-  type: string;
-  icon: string;
-  title?: string;
-  content?: string;
-  override?: any;
-  html?: any;
-  state?: string;
-  createdOn?: Date;
-  destroyedOn?: Date;
+    id?: string
+    type: string
+    icon: string
+    title?: string
+    content?: string
+    override?: any
+    html?: any
+    state?: string
+    createdOn?: Date
+    destroyedOn?: Date
+    animate?: string
+    timeOut?: number
+    maxLength?: number
+    pauseOnHover?: boolean
+    clickToClose?: boolean
+    theClass?: string
 }
--- tests/spec/notifications.service.spec.ts
+++ tests/spec/notifications.service.spec.ts
@@ -46,34 +46,196 @@ describe('NotificationsService', () => {
 
             service.set(defaultNotification, true);
 
-            console.log(notificationEvent);
-
             expect(notificationEvent.command).toBe('set');
             expect(notificationEvent.notification.id !== '0').toBeTruthy();
         })
     );
 
-    it('The following options can be overridden: id, animate, timeOut, pauseOnHover, clickToClose, maxLength, theClass, icon',
+    it('If override id is set its used',
         inject([NotificationsService], (service: NotificationsService) => {
             let notificationEvent: NotificationEvent = null,
-                notificationOverride: Notification = {
-                    id: '1',
-                    animate: 'fromLeft',
-                    timeOut: 500,
-                    pauseOnHover: false,
-                    clickToClose: false,
-                    maxLength: 4,
-                    theClass: 'test',
-                    icon: defaultIcons.alert
-
-                };
+                override = {id: '1'};
 
             service.getChangeEmitter().subscribe(item => notificationEvent = item);
 
-            service.set(notification, true);
+            service.set(Object.assign(defaultNotification, {override: override}), true);
 
-            expect(notificationEvent.command).toBe('set');
-            expect(notificationEvent.notification.id).toBe('2');
+            expect(notificationEvent.notification.id).toBe('1');
+        })
+    );
+
+    it('Success method',
+        inject([NotificationsService], (service: NotificationsService) => {
+            let notificationEvent: NotificationEvent = null;
+            service.getChangeEmitter().subscribe(item => notificationEvent = item);
+
+            let notification: Notification = service.success('Title', 'Message');
+
+            expect(notification.id !== undefined).toBeTruthy();
+            expect(notification.type).toBe('success');
+            expect(notification.icon).toBe(defaultIcons.success);
+
+            expect(notification.title).toBe('Title');
+            expect(notification.content).toBe('Message');
+            expect(notification.override).toBeUndefined();
+            expect(notification.html).toBeUndefined();
+            expect(notification.state).toBeUndefined();
+            expect(notification.createdOn).toBeUndefined();
+            expect(notification.destroyedOn).toBeUndefined();
+        })
+    );
+
+    it('Error method',
+        inject([NotificationsService], (service: NotificationsService) => {
+            let notificationEvent: NotificationEvent = null;
+            service.getChangeEmitter().subscribe(item => notificationEvent = item);
+
+            let notification: Notification = service.error('Title', 'Message');
+
+            expect(notification.id !== undefined).toBeTruthy();
+            expect(notification.type).toBe('error');
+            expect(notification.icon).toBe(defaultIcons.error);
+
+            expect(notification.title).toBe('Title');
+            expect(notification.content).toBe('Message');
+            expect(notification.override).toBeUndefined();
+            expect(notification.html).toBeUndefined();
+            expect(notification.state).toBeUndefined();
+            expect(notification.createdOn).toBeUndefined();
+            expect(notification.destroyedOn).toBeUndefined();
+        })
+    );
+
+
+    it('Alert method',
+        inject([NotificationsService], (service: NotificationsService) => {
+            let notificationEvent: NotificationEvent = null;
+            service.getChangeEmitter().subscribe(item => notificationEvent = item);
+
+            let notification: Notification = service.alert('Title', 'Message');
+
+            expect(notification.id !== undefined).toBeTruthy();
+            expect(notification.type).toBe('alert');
+            expect(notification.icon).toBe(defaultIcons.alert);
+
+            expect(notification.title).toBe('Title');
+            expect(notification.content).toBe('Message');
+            expect(notification.override).toBeUndefined();
+            expect(notification.html).toBeUndefined();
+            expect(notification.state).toBeUndefined();
+            expect(notification.createdOn).toBeUndefined();
+            expect(notification.destroyedOn).toBeUndefined();
+        })
+    );
+
+
+    it('Info method',
+        inject([NotificationsService], (service: NotificationsService) => {
+            let notificationEvent: NotificationEvent = null;
+            service.getChangeEmitter().subscribe(item => notificationEvent = item);
+
+            let notification: Notification = service.info('Title', 'Message');
+
+            expect(notification.id !== undefined).toBeTruthy();
+            expect(notification.type).toBe('info');
+            expect(notification.icon).toBe(defaultIcons.info);
+
+            expect(notification.title).toBe('Title');
+            expect(notification.content).toBe('Message');
+            expect(notification.override).toBeUndefined();
+            expect(notification.html).toBeUndefined();
+            expect(notification.state).toBeUndefined();
+            expect(notification.createdOn).toBeUndefined();
+            expect(notification.destroyedOn).toBeUndefined();
+        })
+    );
+
+
+    it('Bare method',
+        inject([NotificationsService], (service: NotificationsService) => {
+            let notificationEvent: NotificationEvent = null;
+            service.getChangeEmitter().subscribe(item => notificationEvent = item);
+
+            let notification: Notification = service.bare('Title', 'Message');
+
+            expect(notification.id !== undefined).toBeTruthy();
+            expect(notification.type).toBe('bare');
+            expect(notification.icon).toBe('bare');
+
+            expect(notification.title).toBe('Title');
+            expect(notification.content).toBe('Message');
+            expect(notification.override).toBeUndefined();
+            expect(notification.html).toBeUndefined();
+            expect(notification.state).toBeUndefined();
+            expect(notification.createdOn).toBeUndefined();
+            expect(notification.destroyedOn).toBeUndefined();
+        })
+    );
+
+
+    it('Create method',
+        inject([NotificationsService], (service: NotificationsService) => {
+            let notificationEvent: NotificationEvent = null;
+            service.getChangeEmitter().subscribe(item => notificationEvent = item);
+
+            let notification: Notification = service.create('Title', 'Message', 'create');
+
+            expect(notification.id !== undefined).toBeTruthy();
+            expect(notification.type).toBe('create');
+            expect(notification.icon).toBe('bare');
+
+            expect(notification.title).toBe('Title');
+            expect(notification.content).toBe('Message');
+            expect(notification.override).toBeUndefined();
+            expect(notification.html).toBeUndefined();
+            expect(notification.state).toBeUndefined();
+            expect(notification.createdOn).toBeUndefined();
+            expect(notification.destroyedOn).toBeUndefined();
+        })
+    );
+
+
+    it('Html method',
+        inject([NotificationsService], (service: NotificationsService) => {
+            let notificationEvent: NotificationEvent = null;
+            service.getChangeEmitter().subscribe(item => notificationEvent = item);
+
+            let notification: Notification = service.html('<B>Title</B>', 'success');
+
+            expect(notification.id !== undefined).toBeTruthy();
+            expect(notification.type).toBe('success');
+            expect(notification.icon).toBe('bare');
+
+            expect(notification.title).toBeNull();
+            expect(notification.content).toBeNull();
+            expect(notification.override).toBeUndefined();
+            expect(notification.html).toBe('<B>Title</B>');
+            expect(notification.state).toBeUndefined();
+            expect(notification.createdOn).toBeUndefined();
+            expect(notification.destroyedOn).toBeUndefined();
+        })
+    );
+
+    it('Empty remove emits cleanAll command',
+        inject([NotificationsService], (service: NotificationsService) => {
+            let notificationEvent: NotificationEvent = null;
+            service.getChangeEmitter().subscribe(item => notificationEvent = item);
+
+            service.remove();
+
+            expect(notificationEvent.command).toBe('cleanAll');
+        })
+    );
+
+    it('Remove with id emits clean command',
+        inject([NotificationsService], (service: NotificationsService) => {
+            let notificationEvent: NotificationEvent = null;
+            service.getChangeEmitter().subscribe(item => notificationEvent = item);
+
+            service.remove('1');
+
+            expect(notificationEvent.command).toBe('clean');
+            expect(notificationEvent.id).toBe('1');
         })
     );
 
