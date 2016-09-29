f72c85c9305fa57590088b5653dd5c5946784e92
Filip Lauc <filip.lauc93@gmail.com>
2016-09-29 13:48:55 +0200

    test: Working on basic tests

---
+++ spec/max.pipe.spec.ts
@@ -0,0 +1,12 @@
+import {MaxPipe} from '../src/max.pipe';
+
+describe('spec/max.pipe.spec.ts', () => {
+    let pipe: MaxPipe,
+        testString = 'test';
+
+    beforeEach(() => pipe = new MaxPipe());
+
+    it('Less then allowed', () => expect(pipe.transform(testString, testString.length + 1)).toEqual(testString));
+    it('Equal to allowed', () => expect(pipe.transform(testString, testString.length)).toEqual(testString));
+    it('More then allowed', () => expect(pipe.transform(testString, testString.length - 1)).toEqual(testString.slice(0, -1)));
+});
