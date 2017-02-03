c8f2e8bf7712c722cd4627d905bdc23dd52af45e
IT-Ideas <IT-Ideas@users.noreply.github.com>
2017-02-03 14:50:01 +0100

    Updated import accordingly

--- src/simple-notifications.module.ts
+++ src/simple-notifications.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {NgModule, ModuleWithProviders} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NotificationsService} from './notifications.service';
 import {SimpleNotificationsComponent} from './simple-notifications.component';
