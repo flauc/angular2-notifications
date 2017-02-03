ca85d86c11d835ec058b2d28c760e87095c146ff
Filip Lauc <filip.lauc93@gmail.com>
2017-02-03 22:20:13 +0100

    chore(package): updated version :package:

--- components.d.ts
+++ components.d.ts
@@ -2,7 +2,6 @@ export { SimpleNotificationsModule } from './src/simple-notifications.module';
 export { SimpleNotificationsComponent } from './src/simple-notifications.component';
 export { NotificationComponent } from './src/notification.component';
 export { NotificationsService } from './src/notifications.service';
-export { Notification} from './src/notification.type';
 export { MaxPipe } from './src/max.pipe';
 export { PushNotificationsModule } from './src/push-notifications.module';
 export { PushNotificationsService } from './src/push-notifications.service';
