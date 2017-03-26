ba95ec89cadf88d69909c727dd2494b7e0181994
Shibu Lijack <shibulijack@gmail.com>
2017-03-26 19:18:04 +0530

    Angular 4 Compatibility fix

--- src/simple-notifications.module.ts
+++ src/simple-notifications.module.ts
@@ -1,4 +1,5 @@
 import {NgModule, ModuleWithProviders} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CommonModule} from '@angular/common';
 import {NotificationsService} from './notifications.service';
 import {SimpleNotificationsComponent} from './simple-notifications.component';
