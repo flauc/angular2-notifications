5492a5efa21aed62585713dcc935a264ffb264f7
Gregoire Mathonet <gregoire.mathonet.ext@proximus.com>
2017-09-26 09:42:02 +0200

    OnPush"

--- src/simple-notifications/components/notification.component.ts
+++ src/simple-notifications/components/notification.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit, OnDestroy, Input, ViewEncapsulation, NgZone} from '@angular/core';
-import {trigger, state, style, transition, animate} from '@angular/animations';
-import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
-import {Notification} from '../interfaces/notification.type';
-import {NotificationsService} from '../services/notifications.service';
+import { ChangeDetectorRef, Component, Input, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { Notification } from '../interfaces/notification.type';
+import { NotificationsService } from '../services/notifications.service';
 
 @Component({
     selector: 'simple-notification',
@@ -214,7 +214,7 @@ export class NotificationComponent implements OnInit, OnDestroy {
     constructor(
         private notificationService: NotificationsService,
         private domSanitizer: DomSanitizer,
-        private zone: NgZone
+        private changeRef: ChangeDetectorRef
     ) {}
 
     ngOnInit(): void {
@@ -235,7 +235,7 @@ export class NotificationComponent implements OnInit, OnDestroy {
         this.steps = this.timeOut / 10;
         this.speed = this.timeOut / this.steps;
         this.start = new Date().getTime();
-        this.zone.runOutsideAngular(() => this.timer = setTimeout(this.instance, this.speed));
+        this.timer = setTimeout(this.instance, this.speed);
     }
 
     onEnter(): void {
@@ -277,26 +277,23 @@ export class NotificationComponent implements OnInit, OnDestroy {
     }
 
     private instance = () => {
-        this.zone.runOutsideAngular(() => {
-            this.zone.run(() => this.diff = (new Date().getTime() - this.start) - (this.count * this.speed));
+        this.diff = (new Date().getTime() - this.start) - (this.count * this.speed);
 
-            if (this.count++ === this.steps) this.zone.run(() => this.remove());
-            else if (!this.stopTime) {
-                if (this.showProgressBar) this.zone.run(() => this.progressWidth += 100 / this.steps);
+        if (this.count++ === this.steps) this.remove();
+        else if (!this.stopTime) {
+            if (this.showProgressBar) this.progressWidth += 100 / this.steps;
 
-                this.timer = setTimeout(this.instance, (this.speed - this.diff));
-            }
-        })
+            this.timer = setTimeout(this.instance, (this.speed - this.diff));
+        }
+        this.changeRef.detectChanges();
     };
 
     private remove() {
         if (this.animate) {
             this.item.state = this.animate + 'Out';
-            this.zone.runOutsideAngular(() => {
-                setTimeout(() => {
-                    this.zone.run(() => this.notificationService.set(this.item, false))
-                }, 310);
-            })
+            setTimeout(() => {
+                this.notificationService.set(this.item, false);
+            }, 310);
         } else {
             this.notificationService.set(this.item, false);
         }
